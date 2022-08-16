6fe8908a29bc5557755477f849316143f6d3d76e
Diego Obin <2742934200101@ingenieria.usac.edu.gt>
2022-08-16 12:53:01 -0600

    Actualización de ejemplos

---
+++ Python/ejemplo17.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# DATOS DE ENTRADA
+x = np.array([1,2,3,4,5,6,7,8,9,10]) # Valores para el eje X
+y = np.array([3,4,6,10,18,24,32,46,58,62]) # Valores para el eje y
+
+# ANALISIS DE DATOS
+
+
+# DATOS DE SALIDA
+# Estos deben de salir de la Inteligencia Artificial
+datos_salida_x = [12, 16, 19]
+datos_salida_y = []
+predict = 74
+for dato in datos_salida_x:
+    #Aqui se realiza la prediccion
+    # predict = modelo.predict(dato)
+    predict += 9
+    datos_salida_y.append(predict)
+
+# Pasamos de una array simple a un array de numpy
+datos_salida_x = np.array(datos_salida_x)
+datos_salida_y = np.array(datos_salida_y)
+
+plt.plot(x,y,"o", label="Entrada")
+plt.plot(datos_salida_x, datos_salida_y, label="Salida")
+plt.xlabel("Eje X")
+plt.ylabel("Eje Y")
+plt.grid()
+plt.legend()
+plt.show()
+
+
+
+
+# "Examen Final Tercer Bimestre"
+# El resultado de: 6*8/2-6*3
+# <<Tu nombre>>
+
+var1 = "Examen Final Tercer Bimestre"
+print(var1)
+var2 = 6*8/2-6*3
+print(var2)
+var3 = "Diego Obin"
+print(var3)
+
+
